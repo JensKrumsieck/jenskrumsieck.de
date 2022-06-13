b4e87c9011939b1f4ca1ec8d32a783605803f7a0
Jens Krumsieck <jenso666@live.de>
2022-06-13 17:58:25 +0200

    refactor sharing

---
+++ src/lib/components/blocks/ShareButtons.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { page } from "$app/stores";
+  import {
+    FacebookShare,
+    LinkedInShare,
+    PinterestShare,
+    RedditShare,
+    TwitterShare,
+    WhatsAppShare,
+    XingShare,
+  } from "../button/share";
+  import Subtitle from "../typography/Subtitle.svelte";
+  export let title;
+  export let image;
+</script>
+
+<div class="my-6 flex items-end flex-col">
+  <Subtitle>Diesen Beitrag Teilen</Subtitle>
+  <div class="flex space-x-4">
+    <FacebookShare url={$page.url} {title} />
+    <TwitterShare url={$page.url} {title} />
+    <LinkedInShare url={$page.url} {title} />
+    <XingShare url={$page.url} {title} />
+    <RedditShare url={$page.url} {title} />
+    <PinterestShare url={$page.url} {title} media={image} />
+    <WhatsAppShare url={$page.url} {title} />
+  </div>
+</div>
