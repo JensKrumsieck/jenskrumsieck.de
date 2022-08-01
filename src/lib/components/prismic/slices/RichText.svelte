27edd8c3d0f0851907b5cec1cc29a518c307eb8c
Jens Krumsieck <jenso666@live.de>
2022-08-01 22:10:24 +0200

    adjust margins

--- src/lib/components/prismic/slices/RichText.svelte
+++ src/lib/components/prismic/slices/RichText.svelte
@@ -3,6 +3,6 @@
     export let slice
 </script>
 
-<article class="prose max-w-none prose-lg prose-a:text-dark-green hover:prose-a:text-dark">
+<article class="prose max-w-none prose-lg prose-a:text-dark-green hover:prose-a:text-dark mt-5">
     {@html ph.asHTML(slice.primary.content)}
 </article>
