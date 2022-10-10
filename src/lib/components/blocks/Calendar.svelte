33c456e98285ece618b226bed80523a8defd86ce
Jens Krumsieck <jenso666@live.de>
2022-10-10 15:37:52 +0200

    dcsf

--- src/lib/components/blocks/Calendar.svelte
+++ src/lib/components/blocks/Calendar.svelte
@@ -6,8 +6,13 @@
   import relativeTime from "dayjs/plugin/relativeTime.js";
   import "dayjs/locale/de.js";
   import Subtitle from "../typography/Subtitle.svelte";
+  import utc from "dayjs/plugin/utc";
+  import timezone from "dayjs/plugin/timezone";
+  dayjs.extend(timezone);
+  dayjs.extend(utc);
   dayjs.extend(relativeTime);
   dayjs.locale("de");
+  dayjs.tz.setDefault("Europe/Berlin")
 
   const months = [
     "Jan",
@@ -29,7 +34,9 @@
 
 <div class="grid md:grid-cols-2 gap-4">
   {#each dates as event}
-    <div class="flex bg-gray hover:bg-lighter-green hover:shadow-sm rounded-lg p-3">
+    <div
+      class="flex bg-gray hover:bg-lighter-green hover:shadow-sm rounded-lg p-3"
+    >
       <div
         class="flex items-center flex-col justify-center bg-green rounded-lg p-6 w-24 h-24 self-center grow-0 shrink-0"
       >
@@ -38,7 +45,7 @@
         </div>
         <div class="text-xl">
           {months[dayjs(event.start).month()]}
-        </div>             
+        </div>
         <div class="text-xs">
           {dayjs(event.start).year()}
         </div>
@@ -56,9 +63,8 @@
           <div class="flex flex-row items-center">
             <Fa icon={faClock} size="xs" />
             <div class="text-dark font-medium text-xs px-2">
-                {dayjs(event.start).format("DD.MM.YYYY HH:mm")}, Dauer:
-                ca.
-                {dayjs(event.end).from(event.start, true)}
+              {dayjs(event.start).format("DD.MM.YYYY HH:mm")}, Dauer: ca.
+              {dayjs(event.end).from(event.start, true)}
             </div>
           </div>
           {#if event.location != ""}
--- src/routes/calendar.json/+server.ts
+++ src/routes/calendar.json/+server.ts
@@ -1,10 +1,11 @@
 import dayjs from "dayjs";
-import timezone from "dayjs/plugin/timezone"
-import ical from "node-ical";
+import ical, { type DateWithTimeZone } from "node-ical";
 import { CALENDAR } from '$env/static/private'
 import utc from 'dayjs/plugin/utc'
+import timezone from "dayjs/plugin/timezone"
 dayjs.extend(timezone)
 dayjs.extend(utc)
+dayjs.tz.setDefault("Europe/Berlin")
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function GET(event) {
@@ -12,8 +13,9 @@ export async function GET(event) {
     let content = await data.text()
     let cal = ical.parseICS(content);
     let events = sort(cal);
+    events = events.filter((value, index, self) => self.findIndex(t => t.summary == value.summary && dayjs(t.start).format("YYYY-MM-DD") == dayjs(value.start).format("YYYY-MM-DD")) === index)
     let dates = [];
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < 20; i++) {
         if (events[i])
             dates.push(events[i])
     }
@@ -44,26 +46,24 @@ function sort(data: ical.CalendarResponse) {
     ));
 }
 
-function calculateRecurring(event: ical.VEvent): ical.VEvent[] {    
+function calculateRecurring(event: ical.VEvent): ical.VEvent[] {
     const now = new Date();
     const nextY = new Date();
-    nextY.setFullYear(now.getFullYear() + 1);   
+    nextY.setFullYear(now.getFullYear() + 1);
     let dates = event.rrule.between(now, nextY);
     let duration = dayjs(event.start).diff(event.end)
     if (event.exdate) {
         dates = dates.filter(date => (!event.exdate[dayjs(date).format("YYYY-MM-DD")]));
     }
-    let eventTz = event.rrule.origOptions.tzid ?? "Europe/Berlin"
-    let localTz = dayjs.tz.guess()
-    let tz = eventTz == localTz ? eventTz : localTz;
     let newEvents = dates.map(date => {
+        let start = date as DateWithTimeZone   
         return {
             type: 'VEVENT',
-            start: dayjs.tz(date).tz(tz).toDate() as ical.DateWithTimeZone,
+            start: start,
             summary: event.summary,
             description: event.description,
             location: event.location,
-            end: new Date(dayjs(date).valueOf() - duration) as ical.DateWithTimeZone
+            end: new Date(start.valueOf() - duration) as ical.DateWithTimeZone
         } as ical.VEvent
     })
     return newEvents;
