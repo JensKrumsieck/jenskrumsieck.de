bdf0d3cac3c047ed0b653234f7c0876d63df8820
Jens Krumsieck <jenso666@live.de>
2022-11-22 19:08:40 +0100

    Update +page.js

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -6,9 +6,9 @@ import createClient from "$lib/content/prismic";
 export async function load({ fetch, params }) {
     const client = createClient(fetch)
     const posts = await client.getByType('article', { fetchLinks: ['author.name'], orderings: { field: 'my.article.publish_date', direction: 'desc' }, pageSize: 5 })
-    const dates = await (await fetch(`${base}/calendar.json`)).json()
+    //const dates = await (await fetch(`${base}/calendar.json`)).json()
     if (posts) {
-        return { posts, dates }
+        return { posts }//, dates }
     }
     return {
         status: 404
