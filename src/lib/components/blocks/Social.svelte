8d815022847888397e6ea7d8e167e1fa28a19651
Jens Krumsieck <jenso666@live.de>
2023-11-06 23:08:21 +0100

    reorder social icons

--- src/lib/components/blocks/Social.svelte
+++ src/lib/components/blocks/Social.svelte
@@ -6,13 +6,11 @@
 </script>
 
 <div class={clazz + "md:space-y-0 space-y-2"}>
-    <RoundIconButton href="https://instagram.com/jens.ation/" icon={faInstagram} />
-    <RoundIconButton href="https://twitter.com/jens_ation/" icon={faTwitter} />
+    <RoundIconButton href="https://www.linkedin.com/in/jens-krumsieck-849445211" icon={faLinkedin} />
+    <RoundIconButton href="https://www.xing.com/profile/Jens_Krumsieck" icon={faXing} />
     <RoundIconButton href="https://github.com/JensKrumsieck" icon={faGithub} />
-    <RoundIconButton href="https://youtube.com/c/JensKrumsieck" icon={faYoutube} />
-    <RoundIconButton href="https://www.tiktok.com/@jenskrumsieck" icon={faTiktok} />
     <RoundIconButton href="https://www.researchgate.net/profile/Jens-Krumsieck" icon={faResearchgate} />
     <RoundIconButton href="https://orcid.org/0000-0001-6242-5846" icon={faOrcid} />
-    <RoundIconButton href="https://www.xing.com/profile/Jens_Krumsieck" icon={faXing} />
-    <RoundIconButton href="https://www.linkedin.com/in/jens-krumsieck-849445211" icon={faLinkedin} />
+    <RoundIconButton href="https://twitter.com/jens_ation/" icon={faTwitter} />
+    <RoundIconButton href="https://instagram.com/jens.ation/" icon={faInstagram} />
 </div>
