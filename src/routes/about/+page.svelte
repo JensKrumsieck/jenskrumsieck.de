0ba5e5c5f65570c1808a6749ad118355ecb532bb
Jens Krumsieck <jenso666@live.de>
2024-01-22 18:03:27 +0100

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -17,7 +17,7 @@
       <div class="bg-white px-12 py-12 text-lg">
         <p class="font-bold text-xl">Hi, ich bin Jens!</p>
         <p class="my-3">
-          Als Doktorand in <strong>Bioanorganischer Chemie</strong> an der
+          Als Doktorand in <strong>Anorganischer Chemie</strong> an der
           Technischen Universität Braunschweig erforsche ich in der Arbeitsgruppe
           von <strong>Prof. Dr. Martin Bröring</strong> die faszinierende Welt der
           Porphyrinoide. Diese Strukturen, inspiriert von der Natur, stehen hinter
