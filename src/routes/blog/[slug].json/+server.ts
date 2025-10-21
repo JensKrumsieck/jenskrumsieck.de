c93715fb5e0451f41126fbfdb56a84845f173101
JensKrumsieck <j.krumsieck@outlook.de>
2025-10-21 14:13:18 +0200

    minor

--- src/routes/blog/[slug].json/+server.ts
+++ src/routes/blog/[slug].json/+server.ts
@@ -2,17 +2,17 @@ import { json } from '@sveltejs/kit';
 import createClient from "$lib/content/prismic";
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
-export async function GET({ fetch, params }) {
-    const client = createClient(fetch)
+export async function GET({ params }) {
+    const client = createClient()
     const { slug } = params
     try {
-        const post = await client.getByUID('article', slug, { fetchLinks : ['author.name', 'author.about', 'author.avatar'] })
+        const post = await client.getByUID('article', slug, { fetchLinks: ['author.name', 'author.about', 'author.avatar'] })
 
         if (post) {
             return json({ post })
         }
     }
-    catch{
+    catch {
         //does nothing but is needed to catch the 500 thrown by client.getByUID if empty response is given
     }
     return new Response(undefined, { status: 404 })
