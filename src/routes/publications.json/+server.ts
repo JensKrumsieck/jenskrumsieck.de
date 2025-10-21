dd52ac197b668656c8d4167dc317ba16897b5889
JensKrumsieck <j.krumsieck@outlook.de>
2025-10-21 14:02:49 +0200

    fix

--- src/routes/publications.json/+server.ts
+++ src/routes/publications.json/+server.ts
@@ -3,7 +3,7 @@ import createClient from "$lib/content/prismic";
 
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
-export async function GET({ params }) {
+export async function GET({ }) {
     const client = createClient()
     const papers = await client.getByType('research_article', { orderings: { field: 'my.research_article.year', direction: 'desc' } })
     const posters = await client.getByType('research_item', { orderings: { field: 'my.research_item.presentations.start_date', direction: 'desc' } })
